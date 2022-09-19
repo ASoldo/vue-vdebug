75f6ff6201d4c916c4ca7a8fa7ff170545f0989a
ASoldo <soldo.andrej@gmail.com>
2022-09-19 04:45:59 +0200

    Update vdebug.js
    change border to outline for non destructive UI exp

--- vdebug.js
+++ vdebug.js
@@ -5,13 +5,13 @@ export default {
 				switch (binding.arg) {
 
 					case 'wire':
-						el.style.border = `1px dotted limegreen`;
+						el.style.outline = `1px dotted limegreen`;
 						el.style.boxSizing = `border-box`;
 						break;
 
 					case 'color':
 						if (binding.value) {
-							el.style.borderColor = `${binding.value}`;
+							el.style.outlineColor = `${binding.value}`;
 							return;
 						}
 						console.warn("Please use: 'red' or '#0000ff' as an example...");
@@ -27,7 +27,7 @@ export default {
 
 					case 'size':
 						if (binding.value) {
-							el.style.borderWidth = `${binding.value}`;
+							el.style.outlineWidth = `${binding.value}`;
 							return;
 						}
 						console.warn("Please use: 1px 1px as an example...");
@@ -35,7 +35,7 @@ export default {
 
 					case 'style':
 						if (binding.value) {
-							el.style.borderStyle = `${binding.value}`;
+							el.style.outlineStyle = `${binding.value}`;
 							return;
 						}
 						console.warn("Please use: solid, dotted, dashed...");
